39d66a0c2ed5c868f1201bd10e61ae13848fa8d4
Mattis49 <mattisbbn@gmail.com>
2024-02-29 08:56:47 +0100

    Premier commit

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 const dropdowns = Array.from(document.querySelectorAll(".dropdown-btn"));
+const dropdownContainer = document.getElementsByClassName("dropdown-container");
 
-dropdowns.map((dropdown) => {
-  // Code
+dropdowns.forEach((dropdown) => {
+  dropdown.addEventListener("click", () => {
+    const container = dropdown.nextElementSibling;
+    if (container.style.display === "block") {
+      container.style.display = "none";
+    } else {
+      container.style.display = "block";
+    }
+  });
 });
